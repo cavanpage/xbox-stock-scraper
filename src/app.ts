cf7d68ee98f310daea0ed40690ec6e511f88455f
Cavan <cavan100@gmail.com>
2021-02-07 10:23:56 -0800

    remove discord notification on start

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { getAllHandlerConfigs } from "./utilities";
 import { monitor } from "./monitors";
 import { notify } from "./notifications";
 
-notify("Application Started", undefined, false, true);
+notify("Application Started");
 
 try {
   getAllHandlerConfigs().forEach((x) => {
